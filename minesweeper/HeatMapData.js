19c1d8ae33c03c560ef4d4c90b2e054852bf8211
Philip Heimbock <philip.heimbock@hva.nl>
2014-03-26 13:34:40 +0100

    added method to save document

--- minesweeper/HeatMapData.js
+++ minesweeper/HeatMapData.js
@@ -7,44 +7,28 @@ var HeatMapData = (function () {
     var nextId = 1;
 	var bombs = [];
 	
-	/*
-	 * Add Bomb Position
-	 */
-	this.addBombPosition = function(x, y) {
-		if(bombs.length > 0) {
-			var result = $.grep(bombs, function(e){ return (e.posX == x && e.posY == y); });
-			if (result.length == 0) {
-				var bombsLength=bombs.length+1;
-				bombs[bombsLength] = {
-					posX: x,
-					posY: y,
-					count: 1
-				};
-				console.log("Create bomb position: " +bombs[bombsLength].posX+ "/"+bombs[bombsLength].posY +" = " + bombs[bombsLength].count);
-			} else if (result.length == 1) {
-				console.log("Update bomb position");
-				/*bombs[i] = {
-					posX: x,
-					posY: y,
-					count: count++
-				};*/
-			} else {
-			  // multiple items found
-			}
-		} else {
-			bombs[0] = {
-				posX: x,
-				posY: y,
-				count: 1
-			};
-			console.log("Create first bomb position: " +bombs[0].posX+ "/"+bombs[0].posY +" = " + bombs[0].count);
-		}
-	},
+	// CouchDB jQuery-Plugin
+	$.couch.urlPrefix = "http://localhost:5984";
+	var database = "minesweeper";
 	
-	/*
-	 * Remove Bomb Position
-	 */
-	this.removeBombPosition = function(x, y) {
-		console.log("Removed bomb from " +x+ "/"+y);
+	this.saveToCouch = function() {
+		var bombsdata = $(bombs).serializeArray();
+		var x, y;
+		var doc = {
+			width: "null",
+			height: "null",
+			fields: bombsdata,
+			won: false,
+			lastClick: [x,y]
+		};
+
+		$.couch.db(database).saveDoc(doc, {
+			success: function(data) {
+				console.log(data);
+			},
+			error: function(status) {
+				console.log(status);
+			}
+		});
 	}
 });
