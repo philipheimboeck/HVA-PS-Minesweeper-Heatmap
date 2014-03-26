da5a15557c1920c38485eb8a4285f9cc2d174c2a
florianmathis <florian.mathis@hva.nl>
2014-03-26 14:08:00 +0100

    added setter for game properties and save to couchdb

--- minesweeper/HeatMapData.js
+++ minesweeper/HeatMapData.js
@@ -6,20 +6,57 @@ var HeatMapData = (function () {
     // private static
     var nextId = 1;
 	var bombs = [];
+	var width,height, won, lastX, lastY;
+	
+	/*
+	 * Set Game Size
+	 */
+	this.setGameSize = function(w, h) {
+		this.width=w;
+		this.height=h;
+	},
+	
+	/*
+	 * Game lost or won
+	 */
+	this.hasWon = function(val) {
+		this.won = val;
+	},
+	
+	/*
+	 * Set last click
+	 */
+	this.setLastClick = function(x,y) {
+		this.lastX = x;
+		this.lastY = y;
+	},
+	
+	/*
+	 * Save Game Array
+	 */
+	this.saveGameData = function(gameArray) {
+		for(var i=0; i < (this.width*this.height); i++) {
+			var column = i % this.width;
+			var row = (i - column) / this.width;
+			bombs[i] = {
+				x: column,
+				y: row,
+				value: gameArray[i]
+			};
+		}
+	},
 	
 	// CouchDB jQuery-Plugin
 	$.couch.urlPrefix = "http://localhost:5984";
 	var database = "minesweeper";
 	
 	this.saveToCouch = function() {
-		var bombsdata = $(bombs).serializeArray();
-		var x, y;
 		var doc = {
-			width: "null",
-			height: "null",
-			fields: bombsdata,
-			won: false,
-			lastClick: [x,y]
+			width: this.width,
+			height: this.height,
+			fields: bombs,
+			won: this.won,
+			lastClick: [this.lastX, this.lastY]
 		};
 
 		$.couch.db(database).saveDoc(doc, {
