a53577f15c5ba72de5229dcd0bedfca6651c2baf
florianmathis <florian.mathis@hva.nl>
2014-03-26 12:03:40 +0100

    Added HeatMapData class

---
+++ minesweeper/HeatMapData.js
@@ -0,0 +1,17 @@
+
+/*
+ * HeatMapData Class
+ */
+var HeatMapData = Class.create({
+  init: function(name) {
+    this.name = name;
+  },
+  collectData: function() {
+    blabla
+  }
+});
+
+/*
+var hmd = new HeatMapData("Minecore");
+hmd.collectData();
+*/
